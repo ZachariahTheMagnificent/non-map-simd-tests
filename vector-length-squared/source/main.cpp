0a6f6c5a4796f2abb739d60f92e628ebed990301
Zachariah the Magnificent <zachariahthemagnificent@gmail.com>
2019-04-19 08:55:11 +0800

    first implementation

--- vector-length-squared/source/main.cpp
+++ vector-length-squared/source/main.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
 #include <vector>
+#include <random>
+#include <chrono>
 
 constexpr auto cache_line_num_bytes = 64;
 
@@ -68,7 +70,52 @@ using DynamicArray = std::vector<Type, Allocator<Type>>;
 
 int main(const int num_arguments, char** arguments)
 {
-	DynamicArray<float> vectors;
-	std::cout << "Hello world!\n";
+	constexpr auto seed = std::size_t{860374};
+	constexpr auto num_elements = std::size_t{1'000'000};
+	constexpr auto num_iterations = std::size_t{100};
+	constexpr auto min_value = -1000.0f;
+	constexpr auto max_value = 1000.0f;
+	constexpr auto num_vector_components = 3;
+	
+	auto input = DynamicArray<float>(num_elements*num_vector_components);
+	auto rng_engine = std::mt19937_64{seed};
+	const auto random_float = std::uniform_real_distribution{min_value, max_value};
+	for(auto& element : input)
+	{
+		element = random_float(rng_engine);
+	}
+
+	std::cout << "vector-length-squared test!\n";
+
+	auto output = DynamicArray<float>(input.size()/num_vector_components);
+
+	const auto start_time = std::chrono::steady_clock::now();
+
+#if defined SIMD
+#else
+	for(auto iteration_index = std::size_t{}; iteration_index < num_iterations; ++iteration_index)
+	{
+		for(auto element_index = std::size_t{}; element_index < output.size(); ++element_index)
+		{
+			auto length_squared = float{};
+			for(auto component_index = std::size_t{}; component_index < num_vector_components; ++component_index)
+			{
+				const auto component = input[element_index*num_vector_components + component_index];
+				length_squared += component * component;
+			}
+			output[element_index] = length_squared;
+		}
+	}
+#endif
+
+	const auto end_time = std::chrono::steady_clock::now();
+	
+	const auto duration = end_time - start_time;
+	const auto duration_in_seconds = std::chrono::duration<double>{duration}.count();
+
+	std::cout << "Done!\n";
+	std::cout << "First element: " << output.front() << '\n';
+	std::cout << "Last element: " << output.back() << '\n';
+	std::cout << "Time taken: " << duration_in_seconds << "s\n";
 	return EXIT_SUCCESS;
 }
